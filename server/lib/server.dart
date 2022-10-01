91a70b4517e359c35a97c779aa7cdd2c42aac3c6
aplicaciones010001 <capitan.746zu@gmail.com>
2022-10-01 09:51:38 -0500

    prub

--- server/lib/server.dart
+++ server/lib/server.dart
@@ -4,60 +4,64 @@ import 'package:sevr/sevr.dart';
 void start() async {
   // Log into database
   final db = await Db.create(
-      'mongodb+srv://cbuser:cbpass@cluster0.xpkap.mongodb.net/test?retryWrites=true&w=majority');
+      'mongodb://admin:admin@localhost:27017/?authMechanism=DEFAULT');
+  // 'mongodb+srv://cbuser:cbpass@cluster0.xpkap.mongodb.net/test?retryWrites=true&w=majority
+
   await db.open();
   final coll = db.collection('contacts');
+  print(await coll.find().toList());
 
-  // Create server
-  const port = 8081;
-  final serv = Sevr();
 
-  final corsPaths = ['/', '/:id'];
-  for (var route in corsPaths) {
-    serv.options(route, [
-      (req, res) {
-        setCors(req, res);
-        return res.status(200);
-      }
-    ]);
-  }
+//   // Create server
+//   const port = 8081;
+//   final serv = Sevr();
 
-  serv.get('/', [
-    setCors,
-    (ServRequest req, ServResponse res) async {
-      final contacts = await coll.find().toList();
-      return res.status(200).json({'contacts': contacts});
-    }
-  ]);
+//   final corsPaths = ['/', '/:id'];
+//   for (var route in corsPaths) {
+//     serv.options(route, [
+//       (req, res) {
+//         setCors(req, res);
+//         return res.status(200);
+//       }
+//     ]);
+//   }
 
-  serv.post('/', [
-    setCors,
-    (ServRequest req, ServResponse res) async {
-      await coll.save(req.body);
-      return res.json(
-        await coll.findOne(where.eq('name', req.body['name'])),
-      );
-    }
-  ]);
+//   serv.get('/', [
+//     setCors,
+//     (ServRequest req, ServResponse res) async {
+//       final contacts = await coll.find().toList();
+//       return res.status(200).json({'contacts': contacts});
+//     }
+//   ]);
 
-  serv.delete('/:id', [
-    setCors,
-    (ServRequest req, ServResponse res) async {
-      await coll
-          .remove(where.eq('_id', ObjectId.fromHexString(req.params['id'])));
-      return res.status(200);
-    }
-  ]);
+//   serv.post('/', [
+//     setCors,
+//     (ServRequest req, ServResponse res) async {
+//       await coll.save(req.body);
+//       return res.json(
+//         await coll.findOne(where.eq('name', req.body['name'])),
+//       );
+//     }
+//   ]);
 
-  // Listen for connections
-  serv.listen(port, callback: () {
-    print('Server listening on port: $port');
-  });
-}
+//   serv.delete('/:id', [
+//     setCors,
+//     (ServRequest req, ServResponse res) async {
+//       await coll
+//           .remove(where.eq('_id', ObjectId.fromHexString(req.params['id'])));
+//       return res.status(200);
+//     }
+//   ]);
 
-void setCors(ServRequest req, ServResponse res) {
-  res.response.headers.add('Access-Control-Allow-Origin', '*');
-  res.response.headers.add('Access-Control-Allow-Methods', 'GET, POST, DELETE');
-  res.response.headers
-      .add('Access-Control-Allow-Headers', 'Origin, Content-Type');
+//   // Listen for connections
+//   serv.listen(port, callback: () {
+//     print('Server listening on port: $port');
+//   });
+// }
+
+// void setCors(ServRequest req, ServResponse res) {
+//   res.response.headers.add('Access-Control-Allow-Origin', '*');
+//   res.response.headers.add('Access-Control-Allow-Methods', 'GET, POST, DELETE');
+//   res.response.headers
+//       .add('Access-Control-Allow-Headers', 'Origin, Content-Type');
 }
